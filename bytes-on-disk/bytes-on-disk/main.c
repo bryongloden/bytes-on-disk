d528cebcc4febdb058a0001e1a1e5a6f375bc892
EmilyTree1825 <jjohnson@iMac.local>
2015-06-04 11:13:05 -0400

    Summary #1082938306

--- bytes-on-disk/bytes-on-disk/main.c
+++ bytes-on-disk/bytes-on-disk/main.c
@@ -9,7 +9,26 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    FILE * pFile;
+    char buffer[] = { ' ' };
+    pFile = fopen ("myfile.bin", "w+b");
+    int i;
+    for (i=0; i<30439314; i++)
+    {
+        fwrite (buffer, sizeof(char), sizeof(buffer), pFile);
+    }
+    fclose (pFile);
     return 0;
 }
+
+// [1]
+// [2] http://www.programiz.com/c-programming/c-file-input-output
+// [3]
+// [4] http://www.tutorialspoint.com/c_standard_library/c_function_fwrite.htm
+// [5]
+// [6] http://stackoverflow.com/questions/9180006/how-to-use-fread-and-fwrite-functions-to-read-and-write-binary-files
+// [7]
+// [8] http://www.cplusplus.com/reference/cstdio/fwrite/
+// [9]
+// [10] http://www.programmingsimplified.com/c-program-examples
+
