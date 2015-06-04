6c1872e2c0ee833256acef95670000db7980071a
Dr Bryon Christopher <cissp@bryongloden.com>
2015-06-04 09:30:05 -0400

    Summary #0

---
+++ bytes-on-disk/bytes-on-disk/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  bytes-on-disk
+//
+//  Created by Bryon Gloden on 6/4/15.
+//  Copyright (c) 2015 Bryon Gloden. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
